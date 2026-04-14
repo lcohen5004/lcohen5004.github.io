6077ceefb102b111728e84279596a4063ddbecb1
lcohen5004 <lcohen5@lsu.edu>
2026-04-14 15:39:01 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -1,26 +1,3 @@
 var map = L.map('map').setView([38, -95], 4);
 var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var basemap = L.tileLayer(basemapUrl).addTo(map);
-var radarUrl = 'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
-var radarDisplayOptions = {
-  layers: 'nexrad-n0r-900913',
-  format: 'image/png',
-  transparent: true
-};
-var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
-var weatherAlertsUrl = 'https://api.weather.gov/alerts/active?region_type=land';
-$.getJSON(weatherAlertsUrl, function(data) {
- L.geoJSON(data).addTo(map);
-});
-L.geoJSON(data, {
-  style: { color: 'orange' },
-  // more options go here
-}).addTo(map);
-onEachFeature: function(feature, layer) {
-  layer.bindPopup(feature.properties.headline);
-}
-style: function(feature){
-  var alertColor = 'orange';
-  if (feature.properties.severity === 'Severe') alertColor = 'red';
-  return { color: alertColor };
-},
