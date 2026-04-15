9508848a064d13f7a3e61f713046b74c19f01c80
lcohen5004 <lcohen5@lsu.edu>
2026-04-14 21:36:47 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -1,6 +1,8 @@
 var map = L.map('map').setView([38, -95], 4);
-var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-var basemap = L.tileLayer(basemapUrl).addTo(map);
+var basemapUrl = 'https://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}';
+var basemap = L.tileLayer(basemapUrl, {
+  attribution: 'Tiles © Esri'
+}).addTo(map);
 var radarUrl = 'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
 var radarDisplayOptions = {
   layers: 'nexrad-n0r-900913',
@@ -12,9 +14,13 @@ var weatherAlertsUrl = 'https://api.weather.gov/alerts/active?region_type=land';
 $.getJSON(weatherAlertsUrl, function(data) {
   L.geoJSON(data, {
   style: function(feature){
-  var alertColor = 'orange';
-  if (feature.properties.severity === 'Severe') alertColor = 'red';
-  return { color: alertColor };
+      var alertColor = 'orange';
+
+      if (feature.properties.severity === 'Extreme') alertColor = 'purple';   // NEW
+      else if (feature.properties.severity === 'Severe') alertColor = 'red';
+      else if (feature.properties.severity === 'Minor') alertColor = 'yellow'; // NEW
+
+      return { color: alertColor }
 },
   onEachFeature: function(feature, layer) {
   layer.bindPopup(feature.properties.headline);
